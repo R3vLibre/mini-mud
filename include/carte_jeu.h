13e9447aa3ca94dd5e7893ceed798820560a3158
kévin menet <deftroy@live.fr>
2014-11-05 15:19:07 +0100

    ajout de carte_jeu.cpp et carte_jeu.h

---
+++ include/carte_jeu.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Zone
+{
+	public:
+		static int nb_zone;
+		Zone();         // constructeur
+		~Zone();     // destructeur
+	private:
+		int num_zone;
+		std::string description;
+};
+
+class Carte_Jeu
+{
+	public:
+		Carte_Jeu();          // constructeur
+		~Carte_Jeu();      // destructeur
+	private:
+		Zone * p_liste_zones;
+		//chaque zone connait le(s) pointeur(s) de sa (ses) zone(s) adjacente(s)
+};
---
+++ src/carte_jeu.cpp
@@ -0,0 +1,31 @@
+#include "carte_jeu.h"
+
+//carte du monde : initialisation des zones
+Carte_Jeu::Carte_Jeu()
+{	
+	//création de la zone initiale
+	p_liste_zones=new Zone();
+	/*cout << "bonjour et bienvenue dans notre super jeu de la mort qui tue" << endl;*/
+	return;
+}
+
+int Zone::nb_zone=0;
+
+Zone::Zone()
+{
+	//chaque zones 
+	description="Voici la zone Num 1";
+	num_zone=nb_zone++;
+	/* équivalent à:
+	num_zone=nb_zone;
+	nb_zone++;
+	*/
+	
+	/*l'inverse :
+	num_zone=++nb_zone;
+	est équivalent à:
+	nb_zone++;
+	num_zone=nb_zone;
+	*/
+	return;
+}
