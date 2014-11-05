ba47476f054e859e1040f79524234932d43ade11
kévin menet <deftroy@live.fr>
2014-11-05 17:34:00 +0100

    rajout du main_local pour géré au moins la sortie du jeu

---
+++ src/main/main_local.cpp
@@ -0,0 +1,14 @@
+int main(int argc, char *argv[])
+{
+    /* action à prévoir pour main local :
+    moteur = new moteur_jeu();
+    if = new Interface_Utilisateur();
+    //boucle principale
+    while (description != "quitter()")
+    {
+      if.lire_entree();
+      moteur.derouler_jeu();
+    }
+    exit(0);
+    */
+}
