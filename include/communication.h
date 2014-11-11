54f8f0b0dfa6ed7c5372164a231ab451bc7654c8
Florian Marc <florian.marc@gmx.com>
2014-11-11 12:10:13 +0100

    nouveau fichier communication.h dans la nouvelle branche mini-mud-reseau

---
+++ include/communication.h
@@ -0,0 +1,14 @@
+#ifdef __COMMUNICATION_H__ 
+#define __COMMUNICATION_H__
+
+#include <iostream>
+
+//uniquement un .h
+class Communication
+{
+	public:
+	std::string transmettre_action(std::string);
+	std::string transmettre_message(std::string);
+}
+
+#endif 
