ee9484c3be0b6c127873a2962af5b78e3716f956
Florian Marc <florian.marc@gmx.com>
2014-11-12 17:05:07 +0100

    retouche des fichiers include + finition de com_serveur.cpp et com_serveur.h

--- include/interface_utilisateur.h
+++ include/interface_utilisateur.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 //using namespace std;  // Eviter les collisions entre une des mes fonctions qui porterait le meme nom qu'une fonction d'une librairie
 					    // Le fait d'écrire std::max dit au compilateur de chercher explicitement la fonction max du namespace std::. 
 					    // NE JAMAIS METTRE de using namespace std dans le main !!!
@@ -10,5 +12,5 @@ public:
 	std::string entree_clavier;
 	std::string lire_entree();
 	//string actualiser_ecran(string)
-	
+
 };
--- src/communication/local/com_locale.cpp
+++ src/communication/local/com_locale.cpp
@@ -1,1 +1,13 @@
+#include com_locale.h
+
 // Module gérant la communication locale
+
+Com_Locale::transmettre_action(std::string action)
+	{
+	return action;
+	}
+
+Com_Locale::std::string transmettre_message(std::string message)
+	{
+	return message;
+	}
